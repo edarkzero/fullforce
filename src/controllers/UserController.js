f3bc60a3148a0b7cb0eba87ea54f36ca30664936
olivergarciadp <54827647+olivergarciadp@users.noreply.github.com>
2020-03-04 10:01:14 -0500

    FEAT: Connected model with controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,27 +3,67 @@ const Controller = require("./Controller");
 class StatisticController extends Controller {
   constructor() {
     super();
-    //Add Model
+    this.model = require("../models/UserModel");
   }
 
   async index(req, res) {
-      //call model index
+    await this.model
+      .index(
+        req.query.query,
+        req.query.sort,
+        req.query.page,
+        req.query.per_page
+      )
+      .then(modelResponse => {
+        this.handleResponse(res, modelResponse.status, modelResponse.result);
+      })
+      .catch(err => {
+        this.handleResponse(res, err.status, null, err.err);
+      });
   }
 
   async create(req, res) {
-    //call model create
+    await this.model
+      .create(req.body)
+      .then(modelResponse => {
+        this.handleResponse(res, modelResponse.status, modelResponse.result);
+      })
+      .catch(err => {
+        this.handleResponse(res, err.status, null, err.err);
+      });
   }
 
   async update(req, res) {
-    //call model update
+    await this.model
+      .update(req.params.id, req.body)
+      .then(modelResponse => {
+        this.handleResponse(res, modelResponse.status, modelResponse.result);
+      })
+      .catch(err => {
+        this.handleResponse(res, err.status, null, err.err);
+      });
   }
 
   async show(req, res) {
-    //call model show
+    await this.model
+      .show(req.params.id)
+      .then(modelResponse => {
+        this.handleResponse(res, modelResponse.status, modelResponse.result);
+      })
+      .catch(err => {
+        this.handleResponse(res, err.status, null, err.err);
+      });
   }
 
   async delete(req, res) {
-    //call model delete
+    await this.model
+      .delete(req.body.ids)
+      .then(modelResponse => {
+        this.handleResponse(res, modelResponse.status, modelResponse.result);
+      })
+      .catch(err => {
+        this.handleResponse(res, err.status, null, err.err);
+      });
   }
 }
 
